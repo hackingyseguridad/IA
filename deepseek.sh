de7cb7f6ae649936b7e91023bc2180028b9c27a2
hackingyseguridad <antonio_taboada@hotmail.es>
2026-04-29 22:50:17 +0200

    Update deepseek.sh

--- deepseek.sh
+++ deepseek.sh
@@ -1,4 +1,11 @@
+#!/bin/sh
+# Script para DeepSeek IA
 
+if ! command -v ollama &> /dev/null; then
+    echo "Instalando Ollama..."
+    curl -fsSL https://ollama.com/install.sh | sudo sh
+    sudo systemctl start ollama
+fi
 
-
+echo "Ejecutando DeepSeek-R1:1.5b..."
 ollama run deepseek-r1:1.5b
