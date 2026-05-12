7e2091671ff1f20696b34a453d8ee862a6366b39
hackingyseguridad <antonio_taboada@hotmail.es>
2026-05-12 20:18:07 +0200

    Update gemma_3.sh

--- gemma_3.sh
+++ gemma_3.sh
@@ -1,6 +1,7 @@
 
+# Gemini : la mejor por sus peso!
 
-#  modelo gemma3:1b de Google
+#  modelo gemma3:1b de Google, Necesita 2 GB de RAM para funcionar, y su descarga ocupa aproximadamente 815 MB
 
 ollama run gemma3:1b
 
