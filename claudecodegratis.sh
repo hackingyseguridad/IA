3e84509340ad3a363f170bf59d3a3b2b30062fdf
hackingyseguridad <antonio_taboada@hotmail.es>
2026-06-08 16:08:43 +0200

    Update claudecodegratis.sh

--- claudecodegratis.sh
+++ claudecodegratis.sh
@@ -3,9 +3,12 @@
 # Script Bash Shell 1.0.x para ejecutar Claude Code, con ollana
 # ejecuta Claude (sin GPU NVIDIA)
 
-
 # ollama launch claude --config
+# escogemos una opcion para ejecutar claude code online o local impulsado 
 
+
+# CLaude Code impulstado por Gemma4 online!!!
+ollama launch claude --model gemma4:31b-cloud
 
 
 ollama launch claude --model qwen3-coder
