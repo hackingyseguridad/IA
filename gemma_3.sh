62f9a8814669968e70aca246245318beba74c8cd
hackingyseguridad <antonio_taboada@hotmail.es>
2026-06-08 15:18:13 +0200

    Update gemma_3.sh

--- gemma_3.sh
+++ gemma_3.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 # Gemini : la mejor por su peso!
-#  modelo gemma3:1b de Google, Necesita 2 GB de RAM para funcionar, y su descarga ocupa aproximadamente 815 MB
+# modelo gemma3:1b de Google, Necesita 2 GB de RAM para funcionar, y su descarga ocupa aproximadamente 815 MB
+# ollama --version  ( muestra la version de ollla actual instalada )
+# ollama help ( muesta ayuda con las opciones )
+# ollama list  ( muestra los modelos instalados presentes en local )
+# ollama run nombre_fichero_modelo  ( ejecuta el modelo del nombre del fichero )
+# ollama show nombre_fichero_modelo ( muestra informacion del nombre del fichero )
+# ollama pull nombre_fichero_modelo ( re-ejecuta el modelo del nombre del fichero )
+# ollama stop nombre_fichero_modelo ( para la ejecutcion del modelo del nombre del fichero )
+# ollama rm nombre_fichero_modelo ( borra el modelo del nombre del fichero )
+
+
 
 ollama run gemma3:1b
 
