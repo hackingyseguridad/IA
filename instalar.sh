f2453bdbe116b4f4447ef256c4b5373d6750ea73
hackingyseguridad <antonio_taboada@hotmail.es>
2026-06-10 10:01:54 +0200

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -22,8 +22,36 @@ if command -v systemctl >/dev/null 2>&1; then
     fi
 fi
 
+# Instala Cloude CODE
+echo "Instalando Claude ..."
+uname -a
+
+curl -fsSL https://deb.nodesource.com/setup_lts.x | bash -
+apt install -y nodejs
+
+date
+echo
+timedatectl set-timezone Europe/Madrid
+timedatectl set-local-rtc 0
+timedatectl set-local-rtc 1
+timedatectl status
+apt-get install ntpdate
+ntpdate hora.ngn.rima-tde.net
+set - timezone
+timedatectl status
+dpkg --configure -a
+apt-get clean
+apt-get update
+apt-get full-upgrade --fix-missing -y
+apt-get dist-upgrade -y
+apt-get autoremove -y
+apt -y autoclean
+apt update
+apt -f -y install
+
 curl -fsSL https://claude.ai/install.sh | bash
- echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.zshrc && source ~/.zshrc
+
+echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.zshrc && source ~/.zshrc
 
 echo
 echo "... actualizando diccionarios ...  (R) 2025 hackingyseguridad.com "
