cffd7c6deceef787b06c625d3f3627a5df4bf974
hackingyseguridad <antonio_taboada@hotmail.es>
2026-06-30 10:47:19 +0200

    Update claudecode1.sh

--- claudecode1.sh
+++ claudecode1.sh
@@ -37,8 +37,8 @@ sleep 3
 # escogemos una opcion para ejecutar claude code online o local impulsado
 
 # Opcion1
-# Claude Code Impulsado por deepseek-v4-pro:cloud online en la nube !!!
-# ollama launch claude --model deepseek-v4-flash:cloud
+# Claude Code Impulsado por gpt-oss:20b-cloud online en la nube !!!
+# ollama launch claude --model  gpt-oss:20b-cloud
 
 # Opcion2
 # CLaude Code impulsado por Gemma4 online!!!
